f6021ca2f24629470c5791f58d5f7899f3079728
Ezra <ezra@Ezra-Mac-Mini.local>
2018-03-29 17:17:47 -0700

    further refine config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME,
+  community: process.env.COMMUNITY_NAME || 'FlutterPDX',
   // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL,
+  slackUrl: process.env.SLACK_URL || 'http://flutter.slack.com',
   // access token of slack
   // see https://github.com/outsideris/slack-invite-automation#issue-token
   //
